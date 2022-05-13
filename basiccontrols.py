1d921fb80bffdb204c8727adb8c50d4e9c9f5495
sl1mww <shaun.prime@live.com>
2022-05-13 16:31:19 +0900

    Create basiccontrols.py

---
+++ basiccontrols.py
@@ -0,0 +1,71 @@
+import json
+import sys
+import time
+import urllib.request
+import keyboard
+
+headers = {
+'Authorization': 'Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IjExMSJ9.eyJzdWIiOiJlN\
+jkyOWJjYy05NDg1LTRlNjItYWVhYy1jZGYzMDFiMjg3OTMiLCJleHAiOjE2NTgyMTQwNDIsImlz\
+cyI6Imh0dHBzOi8vcHVibGljLmFwaS5haWJvLmNvbSIsImF1ZCI6IjQ1LjU0NzA2MTA2NzUyMDg\
+xOTIiLCJqdGkiOiJhNDA2YmRjMS02Y2JlLTRmM2ItYWMyMC00NzZhMWQ0M2E3MmMiLCJpYXQiOj\
+E2NTA0MzgwNDJ9.Fam_ltjdrPgRrUEpuHD6tjUL7AxbcgDhkxLJru_rhmbhrg02hwVG2gizpucv\
+BbcX6GjT8yeLF5gZFrihLCWP3W-7aNUWT_Nlwv96_UuXaedfDelfeW77M23pbo1K_GJgeUifs0O\
+zaPS83Hinu8gVgZL3f2oM5DjaAdwL4AqcFjxPmnfn-05OLO53k8-ui-qdh1vfBBWesfVHbsy2I9\
+LMTGxtanEnJAIyHNFwKUco5WlNp5Y1aSnZVdoe9RStvmDSI-XLXUrRl1UTwt40XXns5NGPtX4xs\
+n_UXmMKQCjPXIgLIMEENHbpXe1hvfYRs8W58nP_wlA2weHUE2rQyqEEAQ',
+}
+
+BASE_PATH = 'https://public.api.aibo.com/v1'
+DEVICE_ID = "010ed9e5-bc49-40f7-9e42-e7e2d229e305"
+
+    
+def do_action():
+    while True:
+        if keyboard.is_pressed("w"):
+            print("move forward")
+            api_name="move_forward"
+            data = '{"arguments":' '{"WalkSpeed":1,"WalkDistance":0.5}''}'
+        elif keyboard.is_pressed("s"):
+            print("move backwards")
+            api_name="move_forward"
+            data = '{"arguments":' '{"WalkSpeed":1,"WalkDistance":-0.5}''}'
+        elif keyboard.is_pressed("a"):
+            print("move to left")
+            api_name="move_sideways"
+            data = '{"arguments":' '{"WalkSpeed":1,"WalkDistance":0.5}''}'
+        elif keyboard.is_pressed("d"):
+            print("move to right")
+            api_name="move_sideways"
+            data = '{"arguments":' '{"WalkSpeed":1,"WalkDistance":-0.5}''}'
+        
+        post_url = BASE_PATH + '/devices/' + DEVICE_ID + '/capabilities/'+ api_name + '/execute'
+
+        # POST API
+        req = urllib.request.Request(post_url, data.encode(), headers=headers, method='POST')
+        with urllib.request.urlopen(req) as res:
+            response = res.read()
+        post_result = json.loads(response)
+
+        '''
+        executionId = post_result["executionId"]
+
+        # Get Result of API execution
+        get_result_url = BASE_PATH + '/executions/' + executionId
+        TimeOut = 0
+        while True:
+            req = urllib.request.Request(get_result_url, headers=headers, method='GET')
+            with urllib.request.urlopen(req) as res:
+                response = res.read()
+            get_result = json.loads(response)
+            get_status = get_result["status"]
+
+            print(get_status)
+
+        '''
+           
+    
+        
+if __name__ == '__main__':
+        do_action()
+        exit(1)
