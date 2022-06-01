8a58723ab100745183340902465a08823f14fe62
sl1mww <shaun.prime@live.com>
2022-06-01 15:34:22 +0900

    Update basiccontrols.py
    Update 1/6/2023

--- basiccontrols.py
+++ basiccontrols.py
@@ -3,7 +3,9 @@ import sys
 import time
 import urllib.request
 import keyboard
+from curses import BUTTON2_CLICKED
 from tkinter import *
+
 
 headers = {
 'Authorization': 'Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IjExMSJ9.eyJzdWIiOiJlN\
@@ -16,94 +18,120 @@ zaPS83Hinu8gVgZL3f2oM5DjaAdwL4AqcFjxPmnfn-05OLO53k8-ui-qdh1vfBBWesfVHbsy2I9\
 LMTGxtanEnJAIyHNFwKUco5WlNp5Y1aSnZVdoe9RStvmDSI-XLXUrRl1UTwt40XXns5NGPtX4xs\
 n_UXmMKQCjPXIgLIMEENHbpXe1hvfYRs8W58nP_wlA2weHUE2rQyqEEAQ',
 }
-
 BASE_PATH = 'https://public.api.aibo.com/v1'
 DEVICE_ID = "010ed9e5-bc49-40f7-9e42-e7e2d229e305"
+    
+def move_forward():
+    print("move forward")
+    api_name="move_forward"
+    data = '{"arguments":' '{"WalkSpeed":1,"WalkDistance":0.5}''}'
+
+    post_url = BASE_PATH + '/devices/' + DEVICE_ID + '/capabilities/'+ api_name + '/execute'
+    req = urllib.request.Request(post_url, data.encode(), headers=headers, method='POST')
+    
+    with urllib.request.urlopen(req) as res:
+        response = res.read()
+    post_result = json.loads(response)
+
+def move_backwards():
+    print("move backwards")
+    api_name="move_forward"
+    data = '{"arguments":' '{"WalkSpeed":1,"WalkDistance":-0.5}''}'
+
+    post_url = BASE_PATH + '/devices/' + DEVICE_ID + '/capabilities/'+ api_name + '/execute'
+    req = urllib.request.Request(post_url, data.encode(), headers=headers, method='POST')
+    
+    with urllib.request.urlopen(req) as res:
+        response = res.read()
+    post_result = json.loads(response)
+
+def move_left():
+    print("move to left")
+    api_name="move_sideways"
+    data = '{"arguments":' '{"WalkSpeed":2,"WalkDistance":0.5}''}'
+
+    post_url = BASE_PATH + '/devices/' + DEVICE_ID + '/capabilities/'+ api_name + '/execute'
+    req = urllib.request.Request(post_url, data.encode(), headers=headers, method='POST')
+    
+    with urllib.request.urlopen(req) as res:
+        response = res.read()
+    post_result = json.loads(response)
+
+def move_right():
+    print("move to right")
+    api_name="move_sideways"
+    data = '{"arguments":' '{"WalkSpeed":2,"WalkDistance":-0.5}''}'
+    
+    post_url = BASE_PATH + '/devices/' + DEVICE_ID + '/capabilities/'+ api_name + '/execute'
+    req = urllib.request.Request(post_url, data.encode(), headers=headers, method='POST')
+    
+    with urllib.request.urlopen(req) as res:
+        response = res.read()
+    post_result = json.loads(response)
+
+def turn_around_clock():
+    print("turn around clockwise")
+    api_name="turn_around"
+
+    data = '{"arguments":{"TurnSpeed":2,"TurnAngle":-180}}'
+    
+    post_url = BASE_PATH + '/devices/' + DEVICE_ID + '/capabilities/'+ api_name + '/execute'
+    req = urllib.request.Request(post_url, data.encode(), headers=headers, method='POST')
+    
+    with urllib.request.urlopen(req) as res:
+        response = res.read()
+    post_result = json.loads(response)
+
+def turn_around_anticlock():
+    print("turn around anticlockwise")
+    api_name="turn_around"
+
+    data = '{"arguments":{"TurnSpeed":2,"TurnAngle":180}}'
+    
+    post_url = BASE_PATH + '/devices/' + DEVICE_ID + '/capabilities/'+ api_name + '/execute'
+    req = urllib.request.Request(post_url, data.encode(), headers=headers, method='POST')
+    
+    with urllib.request.urlopen(req) as res:
+        response = res.read()
+    post_result = json.loads(response)
 
 def do_action():
-    
     while True:
         if keyboard.is_pressed("w"):
-            print("move forward")
-            api_name="move_forward"
-            data = '{"arguments":' '{"WalkSpeed":1,"WalkDistance":0.5}''}'
-
-            post_url = BASE_PATH + '/devices/' + DEVICE_ID + '/capabilities/'+ api_name + '/execute'
-            req = urllib.request.Request(post_url, data.encode(), headers=headers, method='POST')
-            
-            with urllib.request.urlopen(req) as res:
-                response = res.read()
-            post_result = json.loads(response)
+            move_forward
         if keyboard.is_pressed("s"):
-            print("move backwards")
-            api_name="move_forward"
-            data = '{"arguments":' '{"WalkSpeed":1,"WalkDistance":-0.5}''}'
-
-            post_url = BASE_PATH + '/devices/' + DEVICE_ID + '/capabilities/'+ api_name + '/execute'
-            req = urllib.request.Request(post_url, data.encode(), headers=headers, method='POST')
-            
-            with urllib.request.urlopen(req) as res:
-                response = res.read()
-            post_result = json.loads(response)
+            move_backwards
         if keyboard.is_pressed("a"):
-            print("move to left")
-            api_name="move_sideways"
-            data = '{"arguments":' '{"WalkSpeed":2,"WalkDistance":0.5}''}'
-
-            post_url = BASE_PATH + '/devices/' + DEVICE_ID + '/capabilities/'+ api_name + '/execute'
-            req = urllib.request.Request(post_url, data.encode(), headers=headers, method='POST')
-            
-            with urllib.request.urlopen(req) as res:
-                response = res.read()
-            post_result = json.loads(response)
+            move_left
         if keyboard.is_pressed("d"):
-            print("move to right")
-            api_name="move_sideways"
-            data = '{"arguments":' '{"WalkSpeed":2,"WalkDistance":-0.5}''}'
-            
-            post_url = BASE_PATH + '/devices/' + DEVICE_ID + '/capabilities/'+ api_name + '/execute'
-            req = urllib.request.Request(post_url, data.encode(), headers=headers, method='POST')
-            
-            with urllib.request.urlopen(req) as res:
-                response = res.read()
-            post_result = json.loads(response)
-        
-    
+            move_right
+        if keyboard.is_pressed("l"):
+            turn_around_clock
+        if keyboard.is_pressed("j"):
+            turn_around_anticlock
 
-      
-        # POST API
-     
+root = Tk()
+root.title("Basic W,A,S,D Movements")
 
-        '''
-        executionId = post_result["executionId"]
+def button_move():
+    return
 
-        # Get Result of API execution
-        get_result_url = BASE_PATH + '/executions/' + executionId
-        TimeOut = 0
-        while True:
-            req = urllib.request.Request(get_result_url, headers=headers, method='GET')
-            with urllib.request.urlopen(req) as res:
-                response = res.read()
-            get_result = json.loads(response)
-            get_status = get_result["status"]
+button_w = Button(root, text="W", padx=30, pady=20, command=move_forward)
+button_s = Button(root, text="S", padx=30, pady=20, command=move_backwards)
+button_a = Button(root, text="A", padx=30, pady=20, command=move_left)
+button_d = Button(root, text="D", padx=30, pady=20, command=move_right)
+button_j = Button(root, text="L", padx=30, pady=20, command=turn_around_clock)
+button_l = Button(root, text="J", padx=30, pady=20, command=turn_around_anticlock)
 
-            print(get_status)
+button_w.grid(row=1, column=2)
+button_s.grid(row=2, column=2)
+button_a.grid(row=2, column=1)
+button_d.grid(row=2, column=3)
+button_l.grid(row=2, column=4)
+button_j.grid(row=2, column=5)
 
-        '''
-           
-    
+root.mainloop()
         
 if __name__ == '__main__':
         do_action()
-        exit(1)
-
-
-root = Tk()
-
-myLabel1 = Label(root, text="Hello World!")
-myLabel2 = Label(root, text="My Name is Ian")
-
-myLabel1.grid(row=0, column=0)
-myLabel2.grid(row=1, column=0)
-
-root.mainloop()
+        exit(1)     
